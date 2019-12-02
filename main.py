9c660ed03e968a2aa07c57c37412c94090bbc251
Bradon Thymes <bmt.wcpa@gmail.com>
2019-12-02 16:21:48 -0500

    Added Firebase Config

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template
 
+firebaseConfig = {
+    apiKey: "AIzaSyBRqKEG14eU2yBO39S633fXJCa7BphB9zo",
+    authDomain: "swamii-260617.firebaseapp.com",
+    databaseURL: "https://swamii-260617.firebaseio.com",
+    projectId: "swamii-260617",
+    storageBucket: "swamii-260617.appspot.com",
+    messagingSenderId: "264652459566",
+    appId: "1:264652459566:web:3176279c6aa01377d8d9bf",
+    measurementId: "G-NQCSM517KM"
+  };
 app = Flask(__name__)
 
 
